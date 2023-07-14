99b4c504cfc5c04d8e6527df61136cf17628608a
seoggyui <likppi10@gmail.com>
2023-07-14 16:54:55 +0900

    1. Usecase interface 생략 2. DataStoreProvider 구현 3. DataStoreProviderModule, LocalDataSourceModule, RepositoryModule 구현 4. 의존성 주입

--- data/src/main/kotlin/com/nexters/bandalart/android/data/local/datasource/GuestLoginTokenDataSourceImpl.kt
+++ data/src/main/kotlin/com/nexters/bandalart/android/data/local/datasource/GuestLoginTokenDataSourceImpl.kt
@@ -1,40 +1,17 @@
 package com.nexters.bandalart.android.data.local.datasource
 
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.core.emptyPreferences
-import androidx.datastore.preferences.core.stringPreferencesKey
 import com.nexters.bandalart.android.data.datasource.GuestLoginTokenDataSource
+import com.nexters.bandalart.android.data.local.DataStoreProvider
+import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.map
-import java.io.IOException
 
-class GuestLoginTokenDataSourceImpl(private val dataStore: DataStore<Preferences>) : GuestLoginTokenDataSource {
+class GuestLoginTokenDataSourceImpl @Inject constructor(private val datastoreProvider: DataStoreProvider) : GuestLoginTokenDataSource {
 
-  private companion object KEY {
-    private const val GUEST_LOGIN_TOKEN = "guest_login_token"
+  override suspend fun setGuestLoginToken(guestLoginToken: String) {
+    datastoreProvider.setGuestLoginToken(guestLoginToken)
   }
 
-  private val prefKey = stringPreferencesKey(GUEST_LOGIN_TOKEN)
-
-  override suspend fun set(guestLoginToken: String) {
-    dataStore.edit { preferences ->
-      preferences[prefKey] = guestLoginToken
-    }
-  }
-
-  override suspend fun get(): Flow<Result<String>> {
-    return dataStore.data.catch { exception ->
-      if (exception is IOException) {
-        emit(emptyPreferences())
-      } else {
-        throw exception
-      }
-    }.map { preference ->
-      preference[prefKey]?.let { Result.success(it) }
-        ?: Result.failure(NoSuchFieldError("Getting guest login token is failed"))
-    }
+  override suspend fun getGuestLoginToken(): Flow<Result<String>> {
+    return datastoreProvider.getGuestLoginToken()
   }
 }
--- domain/src/main/kotlin/com/nexters/bandalart/android/domain/usecase/SetGuestLoginTokenUseCaseImpl.kt
+++
@@ -1,9 +0,0 @@
-package com.nexters.bandalart.android.domain.usecase
-
-import com.nexters.bandalart.android.domain.repository.GuestLoginTokenRepository
-
-class SetGuestLoginTokenUseCaseImpl(private val repository: GuestLoginTokenRepository) : SetGuestLoginTokenUseCase {
-  override suspend fun invoke(guestLoginToken: String) {
-    repository.setGuestLoginToken(guestLoginToken)
-  }
-}
