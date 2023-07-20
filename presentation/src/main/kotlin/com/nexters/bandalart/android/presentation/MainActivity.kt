87babbd7e7271cdf044b89181f2e28552b69be29
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-07-21 00:07:34 +0900

    Splash Screen API 적용 (#10)

--- presentation/src/main/kotlin/com/nexters/bandalart/android/presentation/MainActivity.kt
+++ presentation/src/main/kotlin/com/nexters/bandalart/android/presentation/MainActivity.kt
@@ -10,11 +10,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.nexters.bandalart.android.presentation.ui.theme.BandalartTheme
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    installSplashScreen()
     setContent {
       BandalartTheme {
         // A surface container using the 'background' color from the theme
