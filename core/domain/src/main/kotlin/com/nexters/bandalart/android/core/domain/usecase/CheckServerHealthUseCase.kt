25bdb1466fe27f606f34624ce511e52eb534ddc6
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-07-26 21:40:11 +0900

    UseCase class @Singleton annotation 추가

--- core/domain/src/main/kotlin/com/nexters/bandalart/android/core/domain/usecase/CheckServerHealthUseCase.kt
+++ core/domain/src/main/kotlin/com/nexters/bandalart/android/core/domain/usecase/CheckServerHealthUseCase.kt
@@ -2,7 +2,9 @@ package com.nexters.bandalart.android.core.domain.usecase
 
 import com.nexters.bandalart.android.core.domain.repository.ServerHealthCheckRepository
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class CheckServerHealthUseCase @Inject constructor(
   private val repository: ServerHealthCheckRepository,
 ) {
