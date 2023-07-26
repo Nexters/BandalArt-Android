c873e226c21e8f3eab090b3f318dfc25478c1047
LEE SEOK GYU <54509842+likppi10@users.noreply.github.com>
2023-07-27 02:28:03 +0900

    폰트 추가, 색상 추가 (#18)

--- core/designsystem/src/main/kotlin/com/nexters/bandalart/android/core/ui/theme/Color.kt
+++ core/designsystem/src/main/kotlin/com/nexters/bandalart/android/core/ui/theme/Color.kt
@@ -9,3 +9,21 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Gray50 = Color(0xFFF9FAFB)
+val Gray100 = Color(0xFFF3F4F6)
+val Gray200 = Color(0xFFE5E7EB)
+val Gray300 = Color(0xFFD1D5DB)
+val Gray400 = Color(0xFF9CA3AF)
+val Gray500 = Color(0xFF6B7280)
+val Gray600 = Color(0xFF4B5563)
+val Gray700 = Color(0xFF374151)
+val Gray800 = Color(0xFF1F2937)
+val Gray900 = Color(0xFF111827)
+
+val Black = Color(0xFF060A27)
+val Gray = Color(0xFFDDE0FF)
+
+// Todo 색 선택 기능을 추가하면 네이밍이 달라져야 함
+val Primary = Color(0xFF863DFF)
+val Secondary = Color(0xFFCBFF2E)
