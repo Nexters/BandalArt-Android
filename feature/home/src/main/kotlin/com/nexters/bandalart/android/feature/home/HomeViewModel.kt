032013da20f337ec36f62c613cbf22bd08411660
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-07-29 02:37:02 +0900

    HomeScreen 내에 반다라트 표 추가
    메인 셀 조회 API 를 통해 데이터를 조회하여 표에 데이터를 반영

--- feature/home/src/main/kotlin/com/nexters/bandalart/android/feature/home/HomeViewModel.kt
+++ feature/home/src/main/kotlin/com/nexters/bandalart/android/feature/home/HomeViewModel.kt
@@ -2,19 +2,40 @@ package com.nexters.bandalart.android.feature.home
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.nexters.bandalart.android.core.domain.usecase.CheckServerHealthUseCase
+import com.nexters.bandalart.android.core.domain.usecase.GetBandalartMainCellUseCase
+import com.nexters.bandalart.android.feature.home.mapper.toUiModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import timber.log.Timber
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-  private val checkServerHealthUseCase: CheckServerHealthUseCase,
+  private val getBandalartMainCellUseCase: GetBandalartMainCellUseCase,
 ) : ViewModel() {
 
-  fun checkServerHealth() {
+  private val _homeUiState = MutableStateFlow<HomeUiState>(HomeUiState.Loading)
+  val homeUiState: StateFlow<HomeUiState> = _homeUiState.asStateFlow()
+
+  fun getBandalartMainCell(bandalartKey: String) {
     viewModelScope.launch {
-      checkServerHealthUseCase()
+      _homeUiState.value = HomeUiState.Loading
+      val result = getBandalartMainCellUseCase(bandalartKey)
+      when {
+        result.isSuccess && result.getOrNull() != null -> {
+          _homeUiState.value = HomeUiState.Success(result.getOrNull()!!.toUiModel())
+        }
+        result.isSuccess && result.getOrNull() == null -> {
+          Timber.e("Request succeeded but data validation failed")
+        }
+        result.isFailure -> {
+          _homeUiState.value = HomeUiState.Error(result.exceptionOrNull()!!)
+          Timber.e(result.exceptionOrNull())
+        }
+      }
     }
   }
 }
