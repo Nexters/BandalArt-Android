426c8918df18fbec6c610cc5bc59c2168e70dea5
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-07-29 02:22:20 +0900

    Primary, Secondary Color 변경, Font 환경 설정

---
+++ core/designsystem/src/main/kotlin/com/nexters/bandalart/android/core/ui/theme/Font.kt
@@ -0,0 +1,19 @@
+package com.nexters.bandalart.android.core.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import com.nexters.bandalart.android.core.designsystem.R
+
+val pretendard = FontFamily(
+  Font(R.font.pretendard_black, FontWeight.Black, FontStyle.Normal),
+  Font(R.font.pretendard_bold, FontWeight.Bold, FontStyle.Normal),
+  Font(R.font.pretendard_extra_bold, FontWeight.ExtraBold, FontStyle.Normal),
+  Font(R.font.pretendard_extra_light, FontWeight.ExtraLight, FontStyle.Normal),
+  Font(R.font.pretendard_light, FontWeight.Light, FontStyle.Normal),
+  Font(R.font.pretendard_medium, FontWeight.Medium, FontStyle.Normal),
+  Font(R.font.pretendard_regular, FontWeight.Normal, FontStyle.Normal),
+  Font(R.font.pretendard_semi_bold, FontWeight.SemiBold, FontStyle.Normal),
+  Font(R.font.pretendard_thin, FontWeight.Thin, FontStyle.Normal),
+)
