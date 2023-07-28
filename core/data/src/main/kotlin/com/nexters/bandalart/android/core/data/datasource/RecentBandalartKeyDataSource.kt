11b79bf65acf3177f5dca4bde6cc8b9cd1d7a78b
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-07-29 02:26:06 +0900

    DataStore Get 함수 스트림 함수에서 suspend 함수로 변경

--- core/data/src/main/kotlin/com/nexters/bandalart/android/core/data/datasource/RecentBandalartKeyDataSource.kt
+++ core/data/src/main/kotlin/com/nexters/bandalart/android/core/data/datasource/RecentBandalartKeyDataSource.kt
@@ -1,8 +1,6 @@
 package com.nexters.bandalart.android.core.data.datasource
 
-import kotlinx.coroutines.flow.Flow
-
 interface RecentBandalartKeyDataSource {
   suspend fun setRecentBandalartKey(recentBandalartKey: String)
-  fun getRecentBandalartKey(): Flow<String>
+  suspend fun getRecentBandalartKey(): String
 }
