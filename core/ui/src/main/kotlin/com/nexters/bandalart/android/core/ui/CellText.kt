159af72a43a906a8d7843c38eea35e0f230a3afa
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-07-29 02:23:16 +0900

    앱에서 사용할 Component 추가

---
+++ core/ui/src/main/kotlin/com/nexters/bandalart/android/core/ui/CellText.kt
@@ -0,0 +1,32 @@
+package com.nexters.bandalart.android.core.ui
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.nexters.bandalart.android.core.ui.theme.pretendard
+
+@Composable
+fun CellText(
+  modifier: Modifier = Modifier,
+  cellText: String,
+  cellColor: Color,
+  fontWeight: FontWeight,
+) {
+  Text(
+    modifier = modifier.padding(horizontal = 4.dp, vertical = 6.dp),
+    text = cellText,
+    color = cellColor,
+    lineHeight = 16.8.sp,
+    fontFamily = pretendard,
+    fontWeight = fontWeight,
+    fontSize = 12.sp,
+    letterSpacing = (-0.24).sp,
+    textAlign = TextAlign.Center,
+  )
+}
