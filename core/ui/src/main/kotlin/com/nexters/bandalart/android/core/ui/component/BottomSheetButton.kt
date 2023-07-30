ba058d1632c3e9a1d761499834aa96ea74e38467
seoggyui <likppi10@gmail.com>
2023-07-31 03:35:14 +0900

    코드 리뷰 구성 성분이 한개인 경우 개행 안함

--- core/ui/src/main/kotlin/com/nexters/bandalart/android/core/ui/component/BottomSheetButton.kt
+++ core/ui/src/main/kotlin/com/nexters/bandalart/android/core/ui/component/BottomSheetButton.kt
@@ -21,8 +21,7 @@ fun BottomSheetDeleteButton(
   FilledIconButton(
     onClick = { Toast.makeText(context, "삭제 버튼", Toast.LENGTH_SHORT).show() },
     colors = IconButtonColors(Gray200, Gray900, Gray200, Gray900),
-    modifier = modifier
-      .height(55.dp),
+    modifier = modifier.height(55.dp),
   ) {
     BottomSheetButtonText(text = "삭제")
   }
@@ -36,8 +35,7 @@ fun BottomSheetCompleteButton(
   FilledIconButton(
     onClick = { Toast.makeText(context, "완료 버튼", Toast.LENGTH_SHORT).show() },
     colors = IconButtonColors(Gray900, White, Gray200, Gray400),
-    modifier = modifier
-      .height(55.dp),
+    modifier = modifier.height(55.dp),
   ) {
     BottomSheetButtonText(text = "완료")
   }
