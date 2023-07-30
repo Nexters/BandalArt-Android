581efe0004fd4844c314e9ed93aa8c01f8e4dcd8
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-07-30 18:25:11 +0900

    add icon 추가

--- feature/home/src/main/kotlin/com/nexters/bandalart/android/feature/home/HomeTopBar.kt
+++ feature/home/src/main/kotlin/com/nexters/bandalart/android/feature/home/HomeTopBar.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Icon
@@ -57,10 +58,17 @@ internal fun HomeTopBar(
       ) {
         // TODO 반다라트 표가 여러개 일때는 아이콘, 텍스트 변경
         Row(verticalAlignment = Alignment.CenterVertically) {
+          // TODO 해당 icon 빌드시 에러남 교체 필요
+//          val image = painterResource(id = com.nexters.bandalart.android.core.designsystem.R.drawable.ic_add)
+//          Image(
+//            painter = image,
+//            contentDescription = "Add Icon",
+//          )
           Icon(
             imageVector = Icons.Default.Add,
             contentDescription = "Add Icon",
             tint = Gray600,
+            modifier = Modifier.size(20.dp),
           )
           Text(
             text = "추가",
