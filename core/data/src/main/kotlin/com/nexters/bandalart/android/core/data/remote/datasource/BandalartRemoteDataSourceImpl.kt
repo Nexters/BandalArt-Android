95c4d27a5eed7ee5e1f7478b8c590e5a9455b330
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-08-02 16:26:50 +0900

    dataSourceImpl 내 반복되는 ExceptionHandling 모듈화

--- core/data/src/main/kotlin/com/nexters/bandalart/android/core/data/remote/datasource/BandalartRemoteDataSourceImpl.kt
+++ core/data/src/main/kotlin/com/nexters/bandalart/android/core/data/remote/datasource/BandalartRemoteDataSourceImpl.kt
@@ -7,127 +7,53 @@ import com.nexters.bandalart.android.core.data.model.bandalart.BandalartCellResp
 import com.nexters.bandalart.android.core.data.model.bandalart.BandalartDetailResponse
 import com.nexters.bandalart.android.core.data.model.bandalart.BandalartListResponse
 import com.nexters.bandalart.android.core.data.model.bandalart.UpdateBandalartCellRequest
-import com.nexters.bandalart.android.core.data.util.ExceptionWrapper
+import com.nexters.bandalart.android.core.data.util.extension.safeRequest
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
-import io.ktor.client.plugins.ResponseException
 import io.ktor.client.request.delete
 import io.ktor.client.request.get
 import io.ktor.client.request.patch
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import io.ktor.util.InternalAPI
-import java.nio.channels.UnresolvedAddressException
 import javax.inject.Inject
 
-// TODO 반복 되는 코드 모듈화 필요성
-@Suppress("TooGenericExceptionCaught")
 class BandalartRemoteDataSourceImpl @Inject constructor(
   private val client: HttpClient,
 ) : BandalartRemoteDataSource {
   override suspend fun createBandalart() {
-    try {
-      client
-        .post("v1/bandalarts")
-    } catch (exception: UnresolvedAddressException) {
-      throw ExceptionWrapper(message = "No Internet connection", cause = exception)
-    } catch (exception: ResponseException) {
-      throw ExceptionWrapper(
-        statusCode = exception.response.status.value,
-        message = exception.response.status.description,
-        cause = exception,
-      )
-    } catch (exception: Exception) {
-      throw ExceptionWrapper(message = "Unexpected Error", cause = exception)
+    client.safeRequest {
+      post("v1/bandalarts")
     }
   }
 
   override suspend fun getBandalartList(): BandalartListResponse? {
-    return try {
-      client
-        .get("v1/bandalarts")
-        .body()
-    } catch (exception: UnresolvedAddressException) {
-      throw ExceptionWrapper(message = "No Internet connection", cause = exception)
-    } catch (exception: ResponseException) {
-      throw ExceptionWrapper(
-        statusCode = exception.response.status.value,
-        message = exception.response.status.description,
-        cause = exception,
-      )
-    } catch (exception: Exception) {
-      throw ExceptionWrapper(message = "Unexpected Error", cause = exception)
+    return client.safeRequest {
+      get("v1/bandalarts").body()
     }
   }
 
   override suspend fun getBandalartDetail(bandalartKey: String): BandalartDetailResponse? {
-    return try {
-      client
-        .get("v1/bandalarts/$bandalartKey")
-        .body()
-    } catch (exception: UnresolvedAddressException) {
-      throw ExceptionWrapper(message = "No Internet connection", cause = exception)
-    } catch (exception: ResponseException) {
-      throw ExceptionWrapper(
-        statusCode = exception.response.status.value,
-        message = exception.response.status.description,
-        cause = exception,
-      )
-    } catch (exception: Exception) {
-      throw ExceptionWrapper(message = "Unexpected Error", cause = exception)
+    return client.safeRequest {
+      get("v1/bandalarts/$bandalartKey").body()
     }
   }
 
   override suspend fun deleteBandalart(bandalartKey: String) {
-    try {
-      client
-        .delete("v1/bandalarts/$bandalartKey")
-    } catch (exception: UnresolvedAddressException) {
-      throw ExceptionWrapper(message = "No Internet connection", cause = exception)
-    } catch (exception: ResponseException) {
-      throw ExceptionWrapper(
-        statusCode = exception.response.status.value,
-        message = exception.response.status.description,
-        cause = exception,
-      )
-    } catch (exception: Exception) {
-      throw ExceptionWrapper(message = "Unexpected Error", cause = exception)
+    client.safeRequest {
+      delete("v1/bandalarts/$bandalartKey")
     }
   }
 
   override suspend fun getBandalartMainCell(bandalartKey: String): BandalartCellResponse? {
-    return try {
-      client
-        .get("v1/bandalarts/$bandalartKey/cells")
-        .body()
-    } catch (exception: UnresolvedAddressException) {
-      throw ExceptionWrapper(message = "No Internet connection", cause = exception)
-    } catch (exception: ResponseException) {
-      throw ExceptionWrapper(
-        statusCode = exception.response.status.value,
-        message = exception.response.status.description,
-        cause = exception,
-      )
-    } catch (exception: Exception) {
-      throw ExceptionWrapper(message = "Unexpected Error", cause = exception)
+    return client.safeRequest {
+      get("v1/bandalarts/$bandalartKey/cells").body()
     }
   }
 
   override suspend fun getBandalartCell(bandalartKey: String, cellKey: String): BandalartCellResponse? {
-    return try {
-      client
-        .get("v1/bandalarts/$bandalartKey/cells/$cellKey")
-        .body()
-    } catch (exception: UnresolvedAddressException) {
-      throw ExceptionWrapper(message = "No Internet connection", cause = exception)
-    } catch (exception: ResponseException) {
-      throw ExceptionWrapper(
-        statusCode = exception.response.status.value,
-        message = exception.response.status.description,
-        cause = exception,
-      )
-    } catch (exception: Exception) {
-      throw ExceptionWrapper(message = "Unexpected Error", cause = exception)
+    return client.safeRequest {
+      get("v1/bandalarts/$bandalartKey/cells/$cellKey").body()
     }
   }
 
@@ -136,38 +62,16 @@ class BandalartRemoteDataSourceImpl @Inject constructor(
     cellKey: String,
     updateBandalartRequest: UpdateBandalartCellRequest,
   ) {
-    try {
-      client
-        .patch("v1/bandalarts/$bandalartKey/cells/$cellKey") {
-          setBody(updateBandalartRequest)
-        }
-    } catch (exception: UnresolvedAddressException) {
-      throw ExceptionWrapper(message = "No Internet connection", cause = exception)
-    } catch (exception: ResponseException) {
-      throw ExceptionWrapper(
-        statusCode = exception.response.status.value,
-        message = exception.response.status.description,
-        cause = exception,
-      )
-    } catch (exception: Exception) {
-      throw ExceptionWrapper(message = "Unexpected Error", cause = exception)
+    client.safeRequest {
+      patch("v1/bandalarts/$bandalartKey/cells/$cellKey") {
+        setBody(updateBandalartRequest)
+      }
     }
   }
 
   override suspend fun deleteBandalartCell(bandalartKey: String, cellKey: String) {
-    try {
-      client
-        .delete("v1/bandalarts/$bandalartKey/cells/$cellKey")
-    } catch (exception: UnresolvedAddressException) {
-      throw ExceptionWrapper(message = "No Internet connection", cause = exception)
-    } catch (exception: ResponseException) {
-      throw ExceptionWrapper(
-        statusCode = exception.response.status.value,
-        message = exception.response.status.description,
-        cause = exception,
-      )
-    } catch (exception: Exception) {
-      throw ExceptionWrapper(message = "Unexpected Error", cause = exception)
+    client.safeRequest {
+      delete("v1/bandalarts/$bandalartKey/cells/$cellKey")
     }
   }
 }
--- core/data/src/main/kotlin/com/nexters/bandalart/android/core/data/repository/ServerHealthCheckRepositoryImpl.kt
+++ core/data/src/main/kotlin/com/nexters/bandalart/android/core/data/repository/ServerHealthCheckRepositoryImpl.kt
@@ -1,11 +1,9 @@
 package com.nexters.bandalart.android.core.data.repository
 
-import com.nexters.bandalart.android.core.data.util.ExceptionWrapper
+import com.nexters.bandalart.android.core.data.util.extension.safeRequest
 import com.nexters.bandalart.android.core.domain.repository.ServerHealthCheckRepository
 import io.ktor.client.HttpClient
-import io.ktor.client.plugins.ResponseException
 import io.ktor.client.request.get
-import java.nio.channels.UnresolvedAddressException
 import javax.inject.Inject
 
 @Suppress("TooGenericExceptionCaught")
@@ -13,19 +11,8 @@ class ServerHealthCheckRepositoryImpl @Inject constructor(
   private val client: HttpClient,
 ) : ServerHealthCheckRepository {
   override suspend fun checkServerHealth() {
-    try {
-      client
-        .get("/health-check")
-    } catch (exception: UnresolvedAddressException) {
-      throw ExceptionWrapper(message = "No Internet connection", cause = exception)
-    } catch (exception: ResponseException) {
-      throw ExceptionWrapper(
-        statusCode = exception.response.status.value,
-        message = exception.response.status.description,
-        cause = exception,
-      )
-    } catch (exception: Exception) {
-      throw ExceptionWrapper(message = "Unexpected Error", cause = exception)
+    client.safeRequest {
+      get("/health-check")
     }
   }
 }
