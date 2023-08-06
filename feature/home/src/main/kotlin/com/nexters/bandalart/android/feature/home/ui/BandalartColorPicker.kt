7aa47855a24d3f1f5cf9032ade25fca657c95767
seoggyui <likppi10@gmail.com>
2023-08-07 00:56:01 +0900

    스타일 체크

--- feature/home/src/main/kotlin/com/nexters/bandalart/android/feature/home/ui/BandalartColorPicker.kt
+++ feature/home/src/main/kotlin/com/nexters/bandalart/android/feature/home/ui/BandalartColorPicker.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
--- feature/home/src/main/kotlin/com/nexters/bandalart/android/feature/home/ui/BandalartDatePicker.kt
+++ feature/home/src/main/kotlin/com/nexters/bandalart/android/feature/home/ui/BandalartDatePicker.kt
@@ -17,7 +17,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.SheetState
