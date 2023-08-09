66a770e45b8c22cb11e6ed2e4ba1f8fadb415cc1
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-08-10 01:46:27 +0900

    반다라트 공유 API 연동

---
+++ core/domain/src/main/kotlin/com/nexters/bandalart/android/core/domain/entity/BandalartShareEntity.kt
@@ -0,0 +1,16 @@
+package com.nexters.bandalart.android.core.domain.entity
+
+
+/**
+ * 반다라트 공유
+ *
+ * @param shareUrl 공유 URL
+ * @param key share 고유 키
+ * @param endDate share 만료일
+ */
+
+data class BandalartShareEntity(
+  val shareUrl: String,
+  val key: String,
+  val endDate: String,
+)
--- core/domain/src/main/kotlin/com/nexters/bandalart/android/core/domain/usecase/bandalart/ShareBandalartUseCase.kt
+++ core/domain/src/main/kotlin/com/nexters/bandalart/android/core/domain/usecase/bandalart/ShareBandalartUseCase.kt
@@ -1,21 +1,21 @@
 package com.nexters.bandalart.android.core.domain.usecase.bandalart
 
-import com.nexters.bandalart.android.core.domain.entity.BandalartEntity
+import com.nexters.bandalart.android.core.domain.entity.BandalartShareEntity
 import com.nexters.bandalart.android.core.domain.repository.BandalartRepository
 import com.nexters.bandalart.android.core.domain.util.runSuspendCatching
 import java.io.IOException
 import javax.inject.Inject
 import javax.inject.Singleton
 
-private val CreateBandalartResponseIsNull = IOException("Create Bandalart API response is null.")
+private val ShareBandalartResponseIsNull = IOException("Share Bandalart API response is null.")
 
 @Singleton
-class CreateBandalartUseCase @Inject constructor(
+class ShareBandalartUseCase @Inject constructor(
   private val repository: BandalartRepository,
 ) {
-  suspend operator fun invoke(): Result<BandalartEntity> {
+  suspend operator fun invoke(bandalartKey: String): Result<BandalartShareEntity> {
     return runSuspendCatching {
-      repository.createBandalart() ?: throw CreateBandalartResponseIsNull
+      repository.shareBandalart(bandalartKey) ?: throw ShareBandalartResponseIsNull
     }
   }
 }
