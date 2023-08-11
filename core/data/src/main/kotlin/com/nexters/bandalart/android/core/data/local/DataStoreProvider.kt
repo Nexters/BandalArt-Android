dccf9703510bd5bcbf57f4bb3bf5c87dd8365b09
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-08-12 03:54:02 +0900

    목표를 달성한 반다라트의 키를 관리하는 DataStore 환경 구축
    목표 달성 화면을 띄워줘야 할 반다라트와 아닌 반다라트의 구분을 하기 위함

--- core/data/src/main/kotlin/com/nexters/bandalart/android/core/data/local/DataStoreProvider.kt
+++ core/data/src/main/kotlin/com/nexters/bandalart/android/core/data/local/DataStoreProvider.kt
@@ -9,6 +9,8 @@ import java.io.IOException
 import javax.inject.Inject
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.first
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 
 internal class DataStoreProvider @Inject constructor(
   private val dataStore: DataStore<Preferences>,
@@ -17,10 +19,12 @@ internal class DataStoreProvider @Inject constructor(
   companion object {
     private const val GUEST_LOGIN_TOKEN = "guest_login_token"
     private const val RECENT_BANDALART_KEY = "recent_bandalart_key"
+    private const val COMPLETED_BANDALART_LIST_KEY = "completed_bandalart_list_key"
   }
 
   private val prefKeyGuestLoginToken = stringPreferencesKey(GUEST_LOGIN_TOKEN)
   private val prefKeyRecentBandalartKey = stringPreferencesKey(RECENT_BANDALART_KEY)
+  private val prefKeyCompletedBandalartList = stringPreferencesKey(COMPLETED_BANDALART_LIST_KEY)
 
   suspend fun setGuestLoginToken(guestLoginToken: String) {
     dataStore.edit { preferences ->
@@ -45,4 +49,48 @@ internal class DataStoreProvider @Inject constructor(
       if (exception is IOException) emit(emptyPreferences())
       else throw exception
     }.first()[prefKeyRecentBandalartKey] ?: ""
+
+  suspend fun insertCompletedBandalartKey(bandalartKey: String) {
+    dataStore.edit { preferences ->
+      val currentListAsString = preferences[prefKeyCompletedBandalartList] ?: ""
+      val currentList = stringToList(currentListAsString)
+
+      // 키가 이미 존재하지 않는 경우에만 추가
+      if (!currentList.contains(bandalartKey)) {
+        val updatedList = currentList + bandalartKey
+        preferences[prefKeyCompletedBandalartList] = listToString(updatedList)
+      }
+    }
+  }
+
+  suspend fun checkCompletedBandalartKey(bandalartKey: String) = dataStore.data
+    .catch { exception ->
+      if (exception is IOException) emit(emptyPreferences())
+      else throw exception
+    }.first()[prefKeyCompletedBandalartList]?.let { currentListAsString ->
+    val currentList = stringToList(currentListAsString)
+    currentList.contains(bandalartKey)
+  } ?: false
+
+  suspend fun deleteCompletedBandalartKey(bandalartKey: String) {
+    dataStore.edit { preferences ->
+      val currentListAsString = preferences[prefKeyCompletedBandalartList] ?: ""
+      val currentList = stringToList(currentListAsString)
+
+      // 키가 존재하는 경우에만 제거
+      if (currentList.contains(bandalartKey)) {
+        val updatedList = currentList - bandalartKey
+        preferences[prefKeyCompletedBandalartList] = listToString(updatedList)
+      }
+    }
+  }
+
+  private fun listToString(list: List<String>): String {
+    return Json.encodeToString(list)
+  }
+
+  private fun stringToList(data: String): List<String> {
+    if (data.isEmpty()) return emptyList()
+    return Json.decodeFromString(data)
+  }
 }
