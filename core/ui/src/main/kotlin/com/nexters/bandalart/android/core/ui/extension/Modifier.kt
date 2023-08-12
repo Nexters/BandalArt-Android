f3ac83cec8591dc910eb8b088d7c3e9132dfb695
seoggyui <likppi10@gmail.com>
2023-08-12 15:17:49 +0900

    Modifier.noRippelClick 추가 : colorPicker

---
+++ core/ui/src/main/kotlin/com/nexters/bandalart/android/core/ui/extension/Modifier.kt
@@ -0,0 +1,18 @@
+package com.nexters.bandalart.android.core.ui.extension
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+
+@SuppressLint("ModifierFactoryUnreferencedReceiver")
+inline fun Modifier.noRippleClickable(crossinline onClick: () -> Unit): Modifier = composed {
+  clickable(
+    indication = null,
+    interactionSource = remember { MutableInteractionSource() },
+  ) {
+    onClick()
+  }
+}
