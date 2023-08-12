86632d4765e159b1eec8c86f656746c1c8cdc283
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-08-13 01:57:10 +0900

    style check success

--- app/src/main/kotlin/com/nexters/bandalart/android/initialize/FirebaseCrashlyticsInitializer.kt
+++ app/src/main/kotlin/com/nexters/bandalart/android/initialize/FirebaseCrashlyticsInitializer.kt
@@ -5,12 +5,12 @@ import androidx.startup.Initializer
 import com.google.firebase.crashlytics.BuildConfig
 import com.google.firebase.crashlytics.FirebaseCrashlytics
 
-class FirebaseCrashlyticsInitializer: Initializer<Unit> {
-    override fun create(context: Context) {
-        FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(!BuildConfig.DEBUG)
-    }
+class FirebaseCrashlyticsInitializer : Initializer<Unit> {
+  override fun create(context: Context) {
+    FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(!BuildConfig.DEBUG)
+  }
 
-    override fun dependencies(): List<Class<out Initializer<*>>> {
-        return emptyList()
-    }
+  override fun dependencies(): List<Class<out Initializer<*>>> {
+    return emptyList()
+  }
 }
