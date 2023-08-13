92795a377af580767c20fb98f96e1e9a426c3d92
seoggyui <likppi10@gmail.com>
2023-08-13 18:38:00 +0900

    ViewModel에서 String 값들 참조를 위한 StringResource

---
+++ feature/home/src/main/kotlin/com/nexters/bandalart/android/feature/home/ui/StringResource.kt
@@ -0,0 +1,23 @@
+package com.nexters.bandalart.android.feature.home.ui
+
+import android.content.Context
+import androidx.annotation.StringRes
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+
+sealed class StringResource {
+
+  data class ViewModelStringViewModel(val value: String) : StringResource()
+  class StringResourceText(@StringRes val resId: Int) : StringResource()
+
+  @Composable
+  fun asString() = when (this) {
+    is StringResourceText -> stringResource(resId)
+    is ViewModelStringViewModel -> value
+  }
+
+  fun asString(context: Context) = when (this) {
+    is StringResourceText -> context.getString(resId)
+    is ViewModelStringViewModel -> value
+  }
+}
