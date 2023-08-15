2cf434a0df8671c5688248f7cb2581b3df87c6e3
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-08-15 12:28:14 +0900

    DataStore 네이밍 변경
    token 만 저장하는 DataStore 가 아닌 최근에 확인한 반다라크의 키, (키, 달성 여부등을) 저장하기도 하기 때문에 token_datastore 에서 bandalart_datastore 로 변경

--- core/data/src/main/kotlin/com/nexters/bandalart/android/core/data/di/DataStoreProviderModule.kt
+++ core/data/src/main/kotlin/com/nexters/bandalart/android/core/data/di/DataStoreProviderModule.kt
@@ -12,24 +12,16 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
-// DataStore 의 네이밍을 변경하면 데이터가 바뀌므로 임시로 주석 처리
-// private const val BANDALART_DATASTORE = "bandalart_datastore"
-// private val Context.bandalartDataStore: DataStore<Preferences> by preferencesDataStore(name = BANDALART_DATASTORE)
-
-private const val TOKEN_DATASTORE = "token_datastore"
-private val Context.tokenDataStore: DataStore<Preferences> by preferencesDataStore(name = TOKEN_DATASTORE)
+ private const val BANDALART_DATASTORE = "bandalart_datastore"
+ private val Context.bandalartDataStore: DataStore<Preferences> by preferencesDataStore(name = BANDALART_DATASTORE)
 
 @Module
 @InstallIn(SingletonComponent::class)
 internal object DataStoreProviderModule {
 
-//  @Provides
-//  @Singleton
-//  fun providePreferencesDataStore(@ApplicationContext context: Context) = context.bandalartDataStore
-
   @Provides
   @Singleton
-  fun providePreferencesDataStore(@ApplicationContext context: Context) = context.tokenDataStore
+  fun providePreferencesDataStore(@ApplicationContext context: Context) = context.bandalartDataStore
 
   @Provides
   @Singleton
