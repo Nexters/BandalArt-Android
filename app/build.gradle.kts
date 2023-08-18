c9537b6d37dd4c2d0f5b6275d7d4ccc76d080f9a
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-08-19 04:10:52 +0900

    signingConfig 설정, debug 모드에서 난독화, proguard 제거

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,7 @@
 @file:Suppress("UnstableApiUsage", "INLINE_FROM_HIGHER_PLATFORM")
 
+import java.util.Properties
+
 plugins {
   bandalart("android-application")
   bandalart("android-hilt")
@@ -9,6 +11,30 @@ plugins {
 
 android {
   namespace = "com.nexters.bandalart.android"
+
+  signingConfigs {
+    create("release") {
+      val propertiesFile = rootProject.file("keystore.properties")
+      val properties = Properties()
+      properties.load(propertiesFile.inputStream())
+      storeFile = file(properties["STORE_FILE"] as String)
+      storePassword = properties["STORE_PASSWORD"] as String
+      keyAlias = properties["KEY_ALIAS"] as String
+      keyPassword = properties["KEY_PASSWORD"] as String
+    }
+  }
+
+  buildTypes {
+    getByName("release") {
+      isDebuggable = false
+      isMinifyEnabled =  true
+      proguardFiles(
+        getDefaultProguardFile("proguard-android.txt"),
+        "proguard-rules.pro"
+      )
+      signingConfig = signingConfigs.getByName("release")
+    }
+  }
 
   buildFeatures {
     compose = true
