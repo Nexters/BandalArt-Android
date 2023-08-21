6dedf3bd9294230d67c88b882d1cd4c174a7b3f7
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-08-21 14:10:29 +0900

    style check success

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
   buildTypes {
     getByName("release") {
       isDebuggable = false
-      isMinifyEnabled =  true
+      isMinifyEnabled = true
       proguardFiles(
         getDefaultProguardFile("proguard-android.txt"),
         "proguard-rules.pro"
