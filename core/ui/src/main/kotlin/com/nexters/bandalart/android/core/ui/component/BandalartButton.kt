04277d66540b16c1569457cdafb9fddbc08cc1d6
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-08-22 23:45:12 +0900

    반다라트 버튼에 Throttle 적용
    0.5초 동안 연속적으로 버튼의 입력이 들어올 경우 이를 처음을 제외하고 전부 취소 시킴
    바텀시트가 올라올 동안 중복으로 버튼이 호출될 수 있는 상황을 방지

--- core/ui/src/main/kotlin/com/nexters/bandalart/android/core/ui/component/BandalartButton.kt
+++ core/ui/src/main/kotlin/com/nexters/bandalart/android/core/ui/component/BandalartButton.kt
@@ -1,16 +1,20 @@
 package com.nexters.bandalart.android.core.ui.component
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.nexters.bandalart.android.core.ui.extension.clickableSingle
 import com.nexters.bandalart.android.core.ui.extension.nonScaleSp
 import com.nexters.bandalart.android.core.ui.theme.Gray900
 import com.nexters.bandalart.android.core.ui.theme.White
@@ -22,16 +26,16 @@ fun BandalartButton(
   text: String,
   modifier: Modifier = Modifier,
 ) {
-  Button(
-    onClick = onClick,
+  Box(
     modifier = modifier
       .fillMaxWidth()
       .padding(horizontal = 24.dp)
-      .height(56.dp),
-    colors = ButtonDefaults.buttonColors(
-      containerColor = Gray900,
-      contentColor = Gray900,
-    ),
+      .height(56.dp)
+      .clip(shape = RoundedCornerShape(50.dp))
+      .clickableSingle(onClick = onClick)
+      .background(color = Gray900)
+      .padding(16.dp),
+    contentAlignment = Alignment.Center
   ) {
     Text(
       text = text,
