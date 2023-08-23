085dd8812795996e00b250879d973ce86d12ce4a
seoggyui <likppi10@gmail.com>
2023-08-24 03:02:51 +0900

    version up to v1.0.1 versioncode 2

--- build-logic/src/main/kotlin/internal/constants.kt
+++ build-logic/src/main/kotlin/internal/constants.kt
@@ -4,7 +4,7 @@ internal object ApplicationConstants {
   const val MinSdk = 24
   const val TargetSdk = 34
   const val CompileSdk = 34
-  const val VersionCode = 1
+  const val VersionCode = 2
   const val VersionName = "1.0.1"
   val JavaVersion = org.gradle.api.JavaVersion.VERSION_17
   const val JavaVersionAsInt = 17
