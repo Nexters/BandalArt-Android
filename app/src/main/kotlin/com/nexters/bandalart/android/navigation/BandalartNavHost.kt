41745f2f0da4c98b0c486eeceaabe71b572f08ec
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-08-26 00:45:24 +0900

    Custom Splash Screen 추가  (#88)
    * SplashScreen 추가
    
    * 스플래시 스크린 API 동작 중 앱 아이콘이 보이지 않도록 변경
    
    * feature/splash settings.gradle 에 추가
    
    * MainActivity 에서 처리 했던 기능을 SplashScreen 으로 이동
    
    * style check success
    
    * 앱 아이콘의 각 구역별 색상을 스플래시 스크린의 배경색상과 같게 변경
    
    * icon drawable 파일 네이밍 변경

--- app/src/main/kotlin/com/nexters/bandalart/android/navigation/BandalartNavHost.kt
+++ app/src/main/kotlin/com/nexters/bandalart/android/navigation/BandalartNavHost.kt
@@ -5,10 +5,12 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import com.nexters.bandalart.android.feature.complete.navigation.completeScreen
 import com.nexters.bandalart.android.feature.complete.navigation.navigateToComplete
-import com.nexters.bandalart.android.feature.home.navigation.HOME_NAVIGATION_ROUTE
 import com.nexters.bandalart.android.feature.home.navigation.homeScreen
 import com.nexters.bandalart.android.feature.home.navigation.navigateToHome
+import com.nexters.bandalart.android.feature.onboarding.navigation.navigateToOnBoarding
 import com.nexters.bandalart.android.feature.onboarding.navigation.onBoardingScreen
+import com.nexters.bandalart.android.feature.splash.navigation.SPLASH_NAVIGATION_ROUTE
+import com.nexters.bandalart.android.feature.splash.navigation.splashScreen
 import com.nexters.bandalart.android.ui.BandalartAppState
 
 @Composable
@@ -16,14 +18,17 @@ fun BandalartNavHost(
   modifier: Modifier = Modifier,
   appState: BandalartAppState,
   onShowSnackbar: suspend (String) -> Boolean,
-  startDestination: String = HOME_NAVIGATION_ROUTE,
 ) {
   val navController = appState.navController
   NavHost(
     modifier = modifier,
     navController = navController,
-    startDestination = startDestination,
+    startDestination = SPLASH_NAVIGATION_ROUTE,
   ) {
+    splashScreen(
+      navigateToOnBoarding = navController::navigateToOnBoarding,
+      navigateToHome = navController::navigateToHome,
+    )
     onBoardingScreen(
       navigateToHome = navController::navigateToHome,
     )
---
+++ feature/splash/build.gradle.kts
@@ -0,0 +1,32 @@
+@file:Suppress("UnstableApiUsage", "INLINE_FROM_HIGHER_PLATFORM")
+
+plugins {
+  bandalart("android-library")
+  bandalart("android-hilt")
+}
+
+android {
+  namespace = "com.nexters.bandalart.android.feature.splash"
+
+  buildFeatures {
+    compose = true
+    buildConfig = true
+  }
+  composeOptions {
+    kotlinCompilerExtensionVersion = libs.versions.androidx.compose.compiler.get()
+  }
+}
+
+dependencies {
+  implementations(
+    projects.core.designsystem,
+    projects.core.domain,
+    projects.core.ui,
+    libs.androidx.core,
+    libs.androidx.hilt.compose.navigation,
+    libs.lottie.compose,
+    libs.bundles.androidx.compose,
+    libs.bundles.androidx.lifecycle,
+    libs.timber,
+  )
+}
