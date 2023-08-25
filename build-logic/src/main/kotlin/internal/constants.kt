06433edd8c37aff6ecf84fed3f1435153f3f6613
seoggyui <likppi10@gmail.com>
2023-08-26 03:30:08 +0900

    version up to v1.0.2 변경사항이 존재할 유효한 버전이므로 alpha 삭제

--- build-logic/src/main/kotlin/internal/constants.kt
+++ build-logic/src/main/kotlin/internal/constants.kt
@@ -5,7 +5,7 @@ internal object ApplicationConstants {
   const val TargetSdk = 34
   const val CompileSdk = 34
   const val VersionCode = 3
-  const val VersionName = "1.0.1-alpha01"
+  const val VersionName = "1.0.2"
   val JavaVersion = org.gradle.api.JavaVersion.VERSION_17
   const val JavaVersionAsInt = 17
 }
