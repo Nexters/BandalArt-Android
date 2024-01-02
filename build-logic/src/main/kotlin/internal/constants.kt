66c2285fb36e9f145cb4ce6fe95c95eda5aca163
seoggyui <likppi10@gmail.com>
2024-01-02 18:38:03 +0900

    릴리즈를 위한 버전

--- build-logic/src/main/kotlin/internal/constants.kt
+++ build-logic/src/main/kotlin/internal/constants.kt
@@ -4,8 +4,8 @@ internal object ApplicationConstants {
   const val MinSdk = 24
   const val TargetSdk = 34
   const val CompileSdk = 34
-  const val VersionCode = 4
-  const val VersionName = "1.0.3"
+  const val VersionCode = 5
+  const val VersionName = "1.0.4"
   val JavaVersion = org.gradle.api.JavaVersion.VERSION_17
   const val JavaVersionAsInt = 17
 }
