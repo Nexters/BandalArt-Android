174a67a1f98aa018496e6ed7b5b5d13c9e2b64b4
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2024-05-15 06:48:06 +0900

    HandleException interface 추가

---
+++ core/common/src/main/kotlin/com/nexters/bandalart/android/core/common/HandleException.kt
@@ -0,0 +1,35 @@
+package com.nexters.bandalart.android.core.common
+
+import retrofit2.HttpException
+import timber.log.Timber
+import java.net.SocketTimeoutException
+import java.net.UnknownHostException
+
+interface ErrorHandlerActions {
+    fun setServerErrorDialogVisible(flag: Boolean)
+    fun setNetworkErrorDialogVisible(flag: Boolean)
+}
+
+fun handleException(exception: Throwable, actions: ErrorHandlerActions) {
+    when (exception) {
+        is HttpException -> {
+            if (exception.code() in 500..511) {
+                actions.setServerErrorDialogVisible(true)
+            } else {
+                Timber.e(exception)
+            }
+        }
+
+        is UnknownHostException -> {
+            actions.setNetworkErrorDialogVisible(true)
+        }
+
+        is SocketTimeoutException -> {
+            actions.setServerErrorDialogVisible(true)
+        }
+
+        else -> {
+            Timber.e(exception)
+        }
+    }
+}
