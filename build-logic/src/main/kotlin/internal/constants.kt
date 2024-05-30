e539779b9409d375a85b4170fae84a0c175efebf
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2024-05-31 07:09:31 +0900

    app version up

--- build-logic/src/main/kotlin/internal/constants.kt
+++ build-logic/src/main/kotlin/internal/constants.kt
@@ -4,8 +4,8 @@ internal object ApplicationConstants {
   const val MinSdk = 24
   const val TargetSdk = 34
   const val CompileSdk = 34
-  const val VersionCode = 9
-  const val VersionName = "1.0.8"
+  const val VersionCode = 10
+  const val VersionName = "1.0.9"
   val JavaVersion = org.gradle.api.JavaVersion.VERSION_17
   const val JavaVersionAsInt = 17
 }
