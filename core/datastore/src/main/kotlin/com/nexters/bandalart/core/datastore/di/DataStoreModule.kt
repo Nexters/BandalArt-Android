a0d450e2c375ae07ce3716d178cc9809f40f30ac
jihun.lee <jihun.lee@medisolution.co.kr>
2024-12-09 13:55:41 +0900

    [feat] 인앱 업데이트 업데이트 거절 버전을 관리하기 위한 DataStore 환경 구축

--- core/datastore/src/main/kotlin/com/nexters/bandalart/core/datastore/di/DataStoreModule.kt
+++ core/datastore/src/main/kotlin/com/nexters/bandalart/core/datastore/di/DataStoreModule.kt
@@ -5,6 +5,7 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
 import com.nexters.bandalart.core.datastore.BandalartDataStore
+import com.nexters.bandalart.core.datastore.InAppUpdateDataStore
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -13,15 +14,28 @@ import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 private const val BANDALART_DATASTORE = "bandalart_datastore"
+private const val IN_APP_UPDATE_DATASTORE = "in_app_update_datastore"
 private val Context.bandalartDataStore: DataStore<Preferences> by preferencesDataStore(name = BANDALART_DATASTORE)
+private val Context.inAppUpdateDataStore: DataStore<Preferences> by preferencesDataStore(name = IN_APP_UPDATE_DATASTORE)
 
 @Module
 @InstallIn(SingletonComponent::class)
 internal object DataStoreModule {
     @Provides
     @Singleton
-    fun providePreferencesDataStore(@ApplicationContext context: Context) = context.bandalartDataStore
+    @Bandalart
+    fun provideBandalartPreferencesDataStore(@ApplicationContext context: Context) = context.bandalartDataStore
 
     @Provides
-    fun provideDataStore(dataStore: DataStore<Preferences>) = BandalartDataStore(dataStore)
+    @Singleton
+    @InAppUpdate
+    fun provideInAppUpdatePreferencesDataStore(@ApplicationContext context: Context) = context.inAppUpdateDataStore
+
+    @Provides
+    @Singleton
+    fun provideBandalartDataStore(@Bandalart dataStore: DataStore<Preferences>) = BandalartDataStore(dataStore)
+
+    @Provides
+    @Singleton
+    fun provideInAppUpdateDataStore(@InAppUpdate dataStore: DataStore<Preferences>) = InAppUpdateDataStore(dataStore)
 }
---
+++ core/datastore/src/main/kotlin/com/nexters/bandalart/core/datastore/di/DataStoreQualifier.kt
@@ -0,0 +1,11 @@
+package com.nexters.bandalart.core.datastore.di
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class Bandalart
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class InAppUpdate
