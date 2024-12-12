a8fa9c2b3f8bfd6fbc8b78df85d2748fd6b76482
jihun.lee <jihun.lee@medisolution.co.kr>
2024-12-12 15:49:47 +0900

    [feat] enableEdgeToEdge() 적용

--- app/src/main/kotlin/com/nexters/bandalart/MainActivity.kt
+++ app/src/main/kotlin/com/nexters/bandalart/MainActivity.kt
@@ -3,6 +3,7 @@ package com.nexters.bandalart
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.nexters.bandalart.core.designsystem.theme.BandalartTheme
 import com.nexters.bandalart.ui.BandalartApp
@@ -13,6 +14,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             BandalartTheme {
                 BandalartApp()
