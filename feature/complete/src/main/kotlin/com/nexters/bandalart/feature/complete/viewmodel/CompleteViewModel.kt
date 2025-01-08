6039fe0e92d42bbf430ca161dfbaa601f471eae1
jihun.lee <jihun.lee@medisolution.co.kr>
2025-01-08 15:09:30 +0900

    [refactor] CompleteViewModel viewmodel 패키지 내부로 이동
    로그 출력 함수 제거

--- feature/complete/src/main/kotlin/com/nexters/bandalart/feature/complete/viewmodel/CompleteViewModel.kt
+++ feature/complete/src/main/kotlin/com/nexters/bandalart/feature/complete/viewmodel/CompleteViewModel.kt
@@ -1,4 +1,4 @@
-package com.nexters.bandalart.feature.complete
+package com.nexters.bandalart.feature.complete.viewmodel
 
 import android.net.Uri
 import androidx.lifecycle.SavedStateHandle
@@ -7,9 +7,6 @@ import androidx.lifecycle.viewModelScope
 import androidx.navigation.toRoute
 import com.nexters.bandalart.core.domain.repository.BandalartRepository
 import com.nexters.bandalart.core.navigation.Route
-import com.nexters.bandalart.feature.complete.viewmodel.CompleteUiAction
-import com.nexters.bandalart.feature.complete.viewmodel.CompleteUiEvent
-import com.nexters.bandalart.feature.complete.viewmodel.CompleteUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -38,7 +35,6 @@ class CompleteViewModel @Inject constructor(
     val uiEvent = _uiEvent.receiveAsFlow()
 
     init {
-        Timber.d("imageUri: $bandalartChartImageUri")
         initComplete()
         addCompleteBandalartId()
     }
