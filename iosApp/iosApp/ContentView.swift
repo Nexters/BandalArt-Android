ac84b65e0b4275d90dee5771b80ed128a5c7d888
이지훈 <jihun.lee@medisolution.co.kr>
2025-01-22 14:06:36 +0900

    [feat] iosApp 모듈 추가
    해결?

---
+++ iosApp/iosApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iosApp
+//
+//  Created by 이지훈 on 1/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iosApp/iosAppTests/iosAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  iosAppTests.swift
+//  iosAppTests
+//
+//  Created by 이지훈 on 1/22/25.
+//
+
+import Testing
+
+struct iosAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
