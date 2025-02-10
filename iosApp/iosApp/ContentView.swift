e689a9c258cba01e490dbd76026790b2b317a305
easyhooon <mraz3068@gmail.com>
2025-02-10 23:13:53 +0900

    [feat] iosApp 모듈 추가

---
+++ iosApp/iosApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iosApp
+//
+//  Created by 이지훈 on 2/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iosApp/iosApp/iosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosAppApp.swift
+//  iosApp
+//
+//  Created by 이지훈 on 2/10/25.
+//
+
+import SwiftUI
+
+@main
+struct iosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
