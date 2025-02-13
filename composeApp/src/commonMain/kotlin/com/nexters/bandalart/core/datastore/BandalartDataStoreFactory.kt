254623a17857e24dd72b37835be8a10d7259c019
easyhooon <mraz3068@gmail.com>
2025-02-13 20:14:02 +0900

    [chore] 사용하지 않는 코드 제거

--- composeApp/src/commonMain/kotlin/com/nexters/bandalart/core/datastore/BandalartDataStoreFactory.kt
+++ composeApp/src/commonMain/kotlin/com/nexters/bandalart/core/datastore/BandalartDataStoreFactory.kt
@@ -1,10 +1,7 @@
 package com.nexters.bandalart.core.datastore
 
 import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.PreferenceDataStoreFactory
 import androidx.datastore.preferences.core.Preferences
-import okio.Path.Companion.toPath
-import org.koin.core.module.Module
 
 //fun createDataStore(producePath: () -> String): DataStore<Preferences> {
 //    return PreferenceDataStoreFactory.createWithPath(
@@ -22,6 +19,3 @@ expect class BandalartDataStoreFactory {
 
 internal const val BANDALART_DATA_STORE_FILE_NAME = "bandalart.preferences_pb"
 internal const val IN_APP_UPDATE_DATA_STORE_FILE_NAME = "in_app_update.preferences_pb"
-
-// platformModule에 추가
-expect val platformModule: Module
