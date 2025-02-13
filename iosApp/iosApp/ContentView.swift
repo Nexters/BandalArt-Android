908a8a479d4449b5205058703fd04017ee7155b8
easyhooon <jihun.lee@medisolution.co.kr>
2025-02-13 10:15:10 +0900

    [refactor] iOSApp 모듈내 코드 CMP 프로젝트의 형태로 변경

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -5,20 +5,22 @@
 //  Created by 이지훈 on 2/10/25.
 //
 
+import UIKit
 import SwiftUI
+import ComposeApp
+
+struct ComposeView: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> UIViewController {
+        MainViewControllerKt.MainViewController()
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
+}
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        ComposeView()
+                .ignoresSafeArea(edges: .all)
+                .ignoresSafeArea(.keyboard) // Compose has own keyboard handler
     }
-}
-
-#Preview {
-    ContentView()
 }
