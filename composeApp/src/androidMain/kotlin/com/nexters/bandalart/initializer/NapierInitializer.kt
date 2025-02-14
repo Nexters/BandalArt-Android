3dc23bd74336753fc7445c0c6a1fb1bdf5778162
easyhooon <jihun.lee@medisolution.co.kr>
2025-02-14 11:40:24 +0900

    [chore] androidx-startup 라이브러리 사용 코드 주석 처리

--- composeApp/src/androidMain/kotlin/com/nexters/bandalart/initializer/NapierInitializer.kt
+++ composeApp/src/androidMain/kotlin/com/nexters/bandalart/initializer/NapierInitializer.kt
@@ -1,19 +1,19 @@
 package com.nexters.bandalart.initializer
 
-import android.content.Context
-import androidx.startup.Initializer
-import com.nexters.bandalart.BuildConfig
-import io.github.aakira.napier.DebugAntilog
-import io.github.aakira.napier.Napier
-
-class NapierInitializer : Initializer<Unit> {
-    override fun create(context: Context) {
-        if (BuildConfig.DEBUG) {
-            Napier.base(DebugAntilog())
-        }
-    }
-
-    override fun dependencies(): List<Class<out Initializer<*>>> {
-        return emptyList()
-    }
-}
+//import android.content.Context
+//import androidx.startup.Initializer
+//import com.nexters.bandalart.BuildConfig
+//import io.github.aakira.napier.DebugAntilog
+//import io.github.aakira.napier.Napier
+//
+//class NapierInitializer : Initializer<Unit> {
+//    override fun create(context: Context) {
+//        if (BuildConfig.DEBUG) {
+//            Napier.base(DebugAntilog())
+//        }
+//    }
+//
+//    override fun dependencies(): List<Class<out Initializer<*>>> {
+//        return emptyList()
+//    }
+//}
