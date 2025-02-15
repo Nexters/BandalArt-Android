08948b15c46ef3466cea393fb4aba0c01cd85e8c
easyhooon <mraz3068@gmail.com>
2025-02-15 14:27:31 +0900

    [chore] 필요 없는 코드 제거
    Info.plist 파일을 통해 설정함

--- composeApp/src/iosMain/kotlin/com/nexters/bandalart/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/nexters/bandalart/MainViewController.kt
@@ -6,8 +6,6 @@ import com.nexters.bandalart.di.initKoin
 fun MainViewController() = ComposeUIViewController(
     configure = {
         initKoin()
-        // TODO info.plist 파일을 생성해서 적용하는 방법으로 대체
-        enforceStrictPlistSanityCheck = false
     }
 ) { BandalartApp() }
 
