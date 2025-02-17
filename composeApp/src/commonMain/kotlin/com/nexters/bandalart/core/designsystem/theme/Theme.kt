86c0c25820d29ac1e6db5d12610105a615c36080
easyhooon <jihun.lee@medisolution.co.kr>
2025-02-17 14:41:29 +0900

    [fix] system bar 영역 색상 재설정
    라이트 모드 -> Color.White
    다크 모드 -> Gray900

--- composeApp/src/commonMain/kotlin/com/nexters/bandalart/core/designsystem/theme/Theme.kt
+++ composeApp/src/commonMain/kotlin/com/nexters/bandalart/core/designsystem/theme/Theme.kt
@@ -6,63 +6,27 @@ import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.Density
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80,
+    background = Gray900,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40,
+    background = Color.White,
 )
 
 @Composable
 fun BandalartTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    // dynamicColor: Boolean = true,
     content: @Composable () -> Unit,
 ) {
     val colorScheme = when {
-//        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-//            val context = LocalContext.current
-//            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-//        }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
-//    val view = LocalView.current
-//    if (!view.isInEditMode) {
-//        SideEffect {
-//            val window = (view.context as Activity).window
-//            window.statusBarColor = colorScheme.primary.toArgb()
-//            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
-//        }
-//    }
-//
-//    SideEffect {
-//        val window = (view.context as Activity).window
-//
-//        window.statusBarColor = if (darkTheme) Color.Black.toArgb() else Color.White.toArgb()
-//        window.navigationBarColor = if (darkTheme) Color.Black.toArgb() else Color.White.toArgb()
-//
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-//            // remove unnecessary black screen from bars
-//            window.isNavigationBarContrastEnforced = false
-//        }
-//
-//        val windowsInsetsController = WindowCompat.getInsetsController(window, view)
-//
-//        // status bar's icon always visible
-//        windowsInsetsController.isAppearanceLightStatusBars = !darkTheme
-//        windowsInsetsController.isAppearanceLightNavigationBars = !darkTheme
-//    }
 
     CompositionLocalProvider(
         LocalDensity provides Density(density = LocalDensity.current.density, fontScale = 1f),
