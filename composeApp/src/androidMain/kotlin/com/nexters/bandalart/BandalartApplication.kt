9f702699d284a9abf97b71a95394a9ccf0c8dccf
easyhooon <jihun.lee@medisolution.co.kr>
2025-02-18 17:04:05 +0900

    [chore] 필요없는 주석 처리된 코드 제거

--- composeApp/src/androidMain/kotlin/com/nexters/bandalart/BandalartApplication.kt
+++ composeApp/src/androidMain/kotlin/com/nexters/bandalart/BandalartApplication.kt
@@ -6,20 +6,6 @@ import io.github.aakira.napier.DebugAntilog
 import io.github.aakira.napier.Napier
 import org.koin.android.ext.koin.androidContext
 
-//@OptIn(KoinExperimentalAPI::class)
-//class BandalartApplication : Application(), KoinStartup {
-//    override fun onCreate() {
-//        super.onCreate()
-//        if (BuildConfig.DEBUG) {
-//            Napier.base(DebugAntilog())
-//        }
-//
-//
-//    override fun onKoinStartup() = koinConfiguration {
-//        androidContext(this@BandalartApplication)
-//    }
-//}
-
 class BandalartApplication : Application() {
     override fun onCreate() {
         super.onCreate()
